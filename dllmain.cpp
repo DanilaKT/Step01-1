b32e22c54462a1c3bddb021ed2eeeabc5e386bc3
Danila P. <azered8@gmail.com>
2024-06-25 11:42:43 +0300

    Добавьте файлы проекта.

---
+++ dllmain.cpp
@@ -0,0 +1,51 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <aced.h>
+#include <rxregsvc.h>
+
+void initApp();
+void unloadApp();
+void helloWorld();
+
+extern "C" AcRx::AppRetCode
+acrxEntryPoint(AcRx::AppMsgCode msg, void* pkt)
+{
+
+    switch (msg)
+    {
+
+
+    case AcRx::kInitAppMsg:
+        acrxDynamicLinker->unlockApplication(pkt);
+        acrxRegisterAppMDIAware(pkt);
+        initApp();
+        break;
+    case AcRx::kUnloadAppMsg:
+        unloadApp();
+        break;
+    default:
+        break;
+
+
+    }
+
+    return AcRx::kRetOK;
+
+}
+
+void initApp()
+{
+
+    acedRegCmds->addCommand(L"HELLOWORLD_COMMANDS", L"Hello", L"Bonjour", ACRX_CMD_TRANSPARENT, helloWorld);
+
+}
+
+void unloadApp()
+{
+    acedRegCmds->removeGroup(L"HELLOWORLD_COMMANDS");
+}
+
+void helloWorld()
+{
+    acutPrintf(L"\nHello World!");
+}
